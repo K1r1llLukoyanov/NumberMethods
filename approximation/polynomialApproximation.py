edddeb9fd9c09fb6ee7b439153ad06c27ced7222
Kirill Lukoyanov <112931242+K1r1llLukoyanov@users.noreply.github.com>
2022-09-29 14:47:10 +0300

    accuracy checker added

--- approximation/polynomialApproximation.py
+++ approximation/polynomialApproximation.py
@@ -1,5 +1,18 @@
 import numpy as np
+import math
 from matplotlib import pyplot as plt
+
+def accuracuy(X,Y,V):
+    total = 0
+    n = len(X)
+    m = len(V)
+    for j in range(n):
+        y = 0
+        for k in range(m):
+            y += V[k]*(X[j]**(m-1-k))
+        total += (y-Y[j])**2
+    print(math.sqrt(total/n))
+
 
 def polynomial(X, Y, n):
     m = n+1 # n - degree of polynomial, m - number of unknowns
@@ -11,7 +24,7 @@ def polynomial(X, Y, n):
             for k in range(nx):
                 U[i][j] += (X[k])**(2*n - i - j)
         for j in range(nx):
-            B[i] += (X[j]**(2-i))*Y[j]
+            B[i] += (X[j]**(n-i))*Y[j]
     for i in range(m-1):
         j = i
         while(j < m-1 and U[j][i] == 0):
@@ -39,6 +52,7 @@ def polynomial(X, Y, n):
         Ys.append(0)
         for j in range(m):
             Ys[i] += V[j]*x**(n-j)
+    accuracuy(X, Y, V)
     plt.plot(Xs, Ys)
     plt.scatter(X, Y)
     plt.grid()
@@ -47,7 +61,7 @@ def polynomial(X, Y, n):
 def main():
     X = [-2, -1, 0, 1, 2]
     Y = [6, 2, -1, -2, -1]
-    polynomial(X, Y, 2)
+    polynomial(X, Y, 4)
 
 if __name__ == "__main__":
     main()
