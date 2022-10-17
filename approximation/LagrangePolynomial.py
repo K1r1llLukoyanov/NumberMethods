93f18b16d35ee7c3159ff00fcc90087078c510c8
Kirill Lukoyanov <112931242+K1r1llLukoyanov@users.noreply.github.com>
2022-10-17 12:27:39 +0300

    Update LagrangePolynomial.py

--- approximation/LagrangePolynomial.py
+++ approximation/LagrangePolynomial.py
@@ -15,7 +15,7 @@ def lagrange(X, Y):
         denum = 1
         k = 0
         for j in range(len(X)):
-            if i == j:
+            if X[i] == X[j]:
                 continue
             denum *= (X[i] - X[j])
             if(k == 0):
@@ -35,7 +35,8 @@ def lagrange(X, Y):
     final_koefs = [0 for _ in range(len(X))]
     for i in to_find:
         for j in range(len(koefs[i])):
-            final_koefs[j] += Y[i]*koefs[i][j]
+            final_koefs[j] += round(Y[i]*koefs[i][j], 3)
+        
 
     return final_koefs
 
@@ -48,11 +49,11 @@ def printLagrange(L):
         print("x^{}".format(len(L)-1), end=' ')
     elif(L[len(L)-1] == -1):
         print("-x^{}".format(len(L)-1), end=' ')
-    elif(L[0] != 0):
-        if(L[0] > 0):
-            print("{:.2f}*x^{}".format(L[len(L)-1], len(L)-1), end=' ')
+    elif(L[len(L)-1] != 0):
+        if(L[len(L)-1] > 0):
+            print("{:.3f}*x^{}".format(L[len(L)-1], len(L)-1), end=' ')
         else:
-            print("-{:.2f}*x^{}".format(-L[len(L)-1], len(L)-1), end=' ')
+            print("{:.3f}*x^{}".format(-L[len(L)-1], len(L)-1), end=' ')
     for i in range(len(L)-2, 0, -1):
         if (L[i] == 0):
             continue
@@ -60,25 +61,26 @@ def printLagrange(L):
             if L[i] == 1:
                 print("+ x^{}".format(i), end=' ')
             else:
-                print("+ {:.2f}*x^{}".format(L[i], i), end=' ')
+                print("+ {:.3f}*x^{}".format(L[i], i), end=' ')
         else:
             if (L[i] == -1):
                 print("- x^{}".format(i), end=' ')
             else:
-                print("- {:.2f}*x^{}".format(-L[i], i), end=' ')
+                print("- {:.3f}*x^{}".format(-L[i], i), end=' ')
     if (L[0] > 0):
         print("+ {}".format(L[0]))
     elif (L[0] < 0):
         print("- {}".format(-L[0])) 
 
 def main():
-    X = [-1, 0, 0.5, 1]
-    Y = [0, 2, 9/8, 0]
+    X = [-0.16, 74.48, 4.81, 42.16, 29.72, -71.1, 86.9, 10.4, 12.5, 50.1, 64.1, 86.7, 10.4, -12.5, 60.1, 54.1, 76.7, 10.4, 12.5, 50.1, 71.1, 76.9, 20.4, 12.5, 50.1] 
+    Y = [1.13, 14.11, 45.8, 43.62, 92.05, -19.4, 181.0, 13.1, 20.9, -11.8, 14.4, 18.0, 13.1, 19.9, 11.8, 34.4, 28.0, 13.1, 19.9, 11.8, 19.4, 81.0, 23.1, 10.9, 11.8]
     Lk = lagrange(X, Y)
     if(len(Lk) == 0):
         print("Unable to find Lagrange polinomial")
         return
     printLagrange(Lk)
+    print(Lk)
 
 if __name__ == "__main__":
     main()
